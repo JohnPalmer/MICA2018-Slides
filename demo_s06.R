713752d538bcedb12a51e0442c937789139d9dd9
John Palmer <johnrbpalmer@gmail.com>
2018-02-06 14:29:07 +0100

    demo

--- demo_s06.R
+++ demo_s06.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-D_individual = read_csv("http://johnpalmer.github.io/MICA2018-Slides/data/NYC_Crime_Map_2017_NovDec.csv")
+D_individual = read_csv("http://johnpalmer.github.io/MICA2018-Slides/data/NYPD_Complaint_Map__Year_to_Date.csv")
 
 D = D_individual %>% group_by(ADDR_PCT_CD, OFNS_DESC) %>% count()
 
